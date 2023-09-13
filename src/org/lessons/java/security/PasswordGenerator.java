c0b7c08d1a5bda373b59eef373e747ebcc60ede4
andreaconv <andrea2convertini@gmail.com>
2023-09-13 15:50:15 +0200

    snack password

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,8 +1,37 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		
+		Scanner sc = new Scanner(System.in);
+				
+		//nome
+		System.out.print("insierire il proprio nome");
+		String name = sc.nextLine();
+		
+		//cognome
+		System.out.print("insierire il proprio cognome");
+		String lastname = sc.nextLine();
+		
+		//colore
+		System.out.print("insierire il proprio colore preferito");
+		String color = sc.nextLine();
+		
+		//data di nascita
+		//giorno
+		System.out.print("insierire il proprio giorno di nascita a numero");
+		int day = sc.nextInt();
+		//mese
+		System.out.print("insierire il proprio mese di nascita a numero");
+		int month = sc.nextInt();
+		//anno
+		System.out.print("insierire il proprio anno di nascita a numero");
+		int year = sc.nextInt();
+		
+		
+		System.out.print("la tua psw è:" + name + "-" + lastname + "-" + color + "-" + day + "-" + month + "-" + year);
 	}
 }
